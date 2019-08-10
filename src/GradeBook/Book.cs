d96d53123a3ec27dcf2e5c2c0dbd542caf80e4b1
Margarita Artiushenko <margoart.work@gmail.com>
2019-08-10 15:36:05 -0700

    Finished Control flow module.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -13,7 +13,7 @@ namespace GradeBook
 
         public void AddLetterGrade(char letter)
         {
-            switch(letter)
+            switch (letter)
             {
                 case 'A':
                     AddGrade(90);
@@ -27,19 +27,20 @@ namespace GradeBook
                     AddGrade(70);
                     break;
 
-                default:
+                    default:
                     AddGrade(0);
+                    break;
             }
         }
         public void AddGrade(double grade)
         {
-            if(grade <=100 && grade >= 0)
+            if (grade <= 100 && grade >= 0)
             {
                 grades.Add(grade);
             }
             else
             {
-                Console.WriteLine("Invalid Value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
@@ -49,16 +50,42 @@ namespace GradeBook
             result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
-            
+
             for (var index = 0; index < grades.Count; index++)
             {
-              
+
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
                 result.Average += grades[index];
-            } 
-            
+            }
+
             result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
+
+
             return result;
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,15 +10,48 @@ namespace GradeBook
         {
 
             var book = new Book("Margo's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
-            
+            //book.AddGrade(89.1);
+            //book.AddGrade(90.5);
+            //book.AddGrade(77.5);
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a grade or 'q' to quit.");
+                var input = Console.ReadLine();
+
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
+
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The highest grade is {stats.High}.");
             Console.WriteLine($"The lowest grade is {stats.Low}.");
-            Console.WriteLine($"The average grade is {stats.Average}.");
+            Console.WriteLine($"The average grade is {stats.Average:N1}.");
+            Console.WriteLine($"The average grade is {stats.Letter}.");
 
         }
     }
